4d5d56746127039f3734cd6856639f9701c4b2ea
Carmen Peralta <cperalta26@gmail.com>
2019-04-24 13:24:23 -0400

    test(bills): write test of internal api GET route

--- src/server/api/bills.spec.js
+++ src/server/api/bills.spec.js
@@ -1,5 +1,6 @@
 const {expect} = require('chai')
 const request = require('supertest')
+const server = require('../index')
 
 let res = null
 
@@ -7,17 +8,38 @@ describe('Bills routes', () => {
   describe('GET https://api.propublica.org api', () => {
     beforeEach(async () => {
       res = await request('https://api.propublica.org')
-        .get('/congress/v1/115/both/bills/introduced.json')
+        .get('/congress/v1/116/both/bills/introduced.json')
         .set('X-API-Key', 'pmNjGSzuJhAwWaP4Mtcqb09bG93bhsTgBfvNtVnP')
     })
+
     it('returns with a 200 status', () => {
       expect(res.statusCode).to.equal(200)
     })
+
     it('the result body is an object', () => {
       expect(res.body).to.be.an('object')
     })
+
     it('the result returns an array of the last 20 congressional bills', () => {
       expect(res.body.results[0].bills).to.have.lengthOf(20)
     })
-  }) // end describe('/api/bills')
+  }) // end describe('GET https://api.propublica.org api')
+
+  describe('GET /api/bills', () => {
+    beforeEach(async () => {
+      res = await request(server).get('/api/bills')
+    })
+
+    it('returns with a 200 status', () => {
+      expect(res.statusCode).to.equal(200)
+    })
+
+    it('the result has a bills property', () => {
+      expect(res.body.resuts[0].to.have.own.property('bills'))
+    })
+
+    it('the result returns an array of the last 20 congressional bills', () => {
+      expect(res.body.results[0].bills).to.have.lengthOf(20)
+    })
+  }) // end describe('GET /api/bills')
 }) // end describe('Bills routes')
