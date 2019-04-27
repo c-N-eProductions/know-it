67f77206436e8b8617712dde806190bbb6c651d0
Carmen Peralta <cperalta26@gmail.com>
2019-04-27 13:36:19 -0400

    feat(bills.js): create bills api route

--- src/server/api/bills.js
+++ src/server/api/bills.js
@@ -1,3 +1,25 @@
 const router = require('express').Router()
+const https = require('https')
 
 module.exports = router
+
+router.get('/', async (req, res, next) => {
+  const options = {
+    host: 'api.propublica.org',
+    path: '/congress/v1/116/both/bills/introduced.json',
+    headers: {'x-api-key': 'pmNjGSzuJhAwWaP4Mtcqb09bG93bhsTgBfvNtVnP'}
+  }
+
+  let billsData = ''
+  await https.get(options, resp => {
+    // A chunk of data has been received.
+    resp.on('data', bills => {
+      billsData += bills
+    })
+
+    // The whole response has been received.
+    resp.on('end', () => {
+      res.status(200).send(JSON.parse(billsData))
+    })
+  })
+})
