f42b9bf0d9e7e851b083c101fe5b26e2d68584c3
Eric Garnett <egcodes@gmail.com>
2019-05-07 13:57:24 -0400

    feat(billStore): set up initial state, action type, action creator

--- src/client/store/billsStore.js
+++ src/client/store/billsStore.js
@@ -1,8 +1,14 @@
 // initial state
+const recentBills = []
 
 // action types
+export const GET_RECENT_BILLS = 'GET_RECENT_BILLS'
 
 // action creators
+export const getRecentBills = recentBills => ({
+  type: GET_RECENT_BILLS,
+  recentBills
+})
 
 // thunk creators
 
