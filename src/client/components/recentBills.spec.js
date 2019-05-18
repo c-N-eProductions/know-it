6da6c87feddb810467f796ad6cf002a21f7fe2a0
Eric Garnett <egcodes@gmail.com>
2019-05-18 14:15:47 -0400

    test(recentBills.spec): create Recent Bills describe and import testing prop data

--- src/client/components/recentBills.spec.js
+++ src/client/components/recentBills.spec.js
@@ -2,5 +2,14 @@ import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { RecentBills } from './RecentBills'
+import recentBillsData from '../testingData/recentBillsData'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+describe('RecentBills component', () => {
+  let recentBills
+
+  beforeEach(() => {
+    recentBills = shallow(<RecentBills recentBillsData={recentBillsData} />)
+  })
+}) // end of RecentBills component
