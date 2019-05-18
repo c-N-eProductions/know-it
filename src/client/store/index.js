5b8c604456358b6c5004790a3ac05154ea67499d
Carmen Peralta <cperalta26@gmail.com>
2019-05-18 12:53:48 -0400

    feat(store index): add billsReducer to combineReducers

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -1,11 +1,12 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import billsReducer from './billsStore'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({ billsReducer })
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 )
 const store = createStore(reducer, middleware)
 
