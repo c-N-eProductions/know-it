fc53fd3013734113982f094b79fd73ff2bebb267
Eric Garnett <egcodes@gmail.com>
2019-05-24 17:15:37 -0400

    test(recentBill.spec): create title and number of recent bills tests

--- src/client/components/recentBills.spec.js
+++ src/client/components/recentBills.spec.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-// import { RecentBills } from './RecentBills'
-import recentBillsData from '../testingData/recentBillsData'
+import { RecentBills } from './RecentBills'
+import recentBills from '../testingData/recentBillsData'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('RecentBills component', () => {
-  let recentBills
-
+  let recentBillsWrapper
   beforeEach(() => {
-    recentBills = shallow(<RecentBills recentBillsData={recentBillsData} />)
+    recentBillsWrapper = shallow(<RecentBills recentBillsData={recentBills} />)
+  })
+
+  it('should render a title as an h1 element', () => {
+    expect(recentBillsWrapper.find('h1').text()).to.be.equal(
+      '20 Most Recent Congressional Bills'
+    )
+  })
+
+  it('should render twenty recent bills, each with a class of "recentBill"', () => {
+    expect(recentBillsWrapper.find('.recentBill').to.have.lengthOf(20))
   })
 }) // end of RecentBills component
