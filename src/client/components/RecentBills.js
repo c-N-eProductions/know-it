8cb297425367e0611e54cf973b4cf480f0f7c41d
Carmen Peralta <cperalta26@gmail.com>
2019-05-24 18:28:41 -0400

    feat(RecentBills): convert component to a class and fetch recent bills on mount

--- src/client/components/RecentBills.js
+++ src/client/components/RecentBills.js
@@ -1,35 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { fetchRecentBillsThunk } from '../store/billsStore'
 
 /**
  * COMPONENT
  */
-export const RecentBills = ({ recentBills }) => (
-  <div className="recent-bills">
-    {recentBills &&
-      recentBills.map(bill => (
-        <div key={bill.number} className="recent-bill">
-          <ul>
-            <li className="bill-number">
-              <a href={bill.govtrack_url}>{bill.number}</a>
-            </li>
-            <li className="bill-title">{bill.title}</li>
-            <li className="bill-introduced-date">{bill.introuced_date}</li>
-            <li className="bill-sponsor">
-              {`${bill.sponsor_title} ${bill.sponsor_name}: ${
-                bill.sponsor_state
-              } ${bill.sponsor_party}`}
-            </li>
-            <li className="bill-lastest-action">
-              {`${bill.lastest_major_action} on ${
-                bill.lastest_major_action_date
-              }`}
-            </li>
-          </ul>
-        </div>
-      ))}
-  </div>
-)
+export class RecentBills extends Component {
+  constructor() {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.props.getRecentBills()
+  }
+
+  render() {
+    const { recentBills } = props
+    return (
+      <div className="recent-bills">
+        {recentBills &&
+          recentBills.map(bill => (
+            <div key={bill.number} className="recent-bill">
+              <ul>
+                <li className="bill-number">
+                  <a href={bill.govtrack_url}>{bill.number}</a>
+                </li>
+                <li className="bill-title">{bill.title}</li>
+                <li className="bill-introduced-date">{bill.introuced_date}</li>
+                <li className="bill-sponsor">
+                  {`${bill.sponsor_title} ${bill.sponsor_name}: ${
+                    bill.sponsor_state
+                  } ${bill.sponsor_party}`}
+                </li>
+                <li className="bill-lastest-action">
+                  {`${bill.lastest_major_action} on ${
+                    bill.lastest_major_action_date
+                  }`}
+                </li>
+              </ul>
+            </div>
+          ))}
+      </div>
+    )
+  }
+}
 
 /**
  * CONTAINER
@@ -38,4 +52,10 @@ const mapStateToProps = state => ({
   recentBills: state.recentBills
 })
 
-export default connect(mapStateToProps)(RecentBills)
+const mapDispatchToProps = dispatch => ({
+  getRecentBills() {
+    dispatch(fetchRecentBillsThunk())
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecentBills)
