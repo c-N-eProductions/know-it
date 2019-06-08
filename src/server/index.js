d12d3c55607d544e06d919abb9aac4da5fdf50d4
Carmen Peralta <cperalta26@gmail.com>
2019-06-08 12:21:00 -0400

    fix(server/index): location path to serve index.html dquote> dquote> dquote> Co-authored-by: Carmen Peralta <cperalta26@gmail.com> Co-authored-by: Eric Garnett <egcodes@gmail.com>

--- src/server/index.js
+++ src/server/index.js
@@ -13,7 +13,7 @@ const createApp = () => {
 
   // body parsing middleware
   app.use(express.json())
-  app.use(express.urlencoded({extended: true}))
+  app.use(express.urlencoded({ extended: true }))
 
   // compression middleware
   app.use(compression())
@@ -40,7 +40,7 @@ const createApp = () => {
 
   // sends index.html
   app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+    res.sendFile(path.join(__dirname, '../..', 'public/index.html'))
   })
 
   // error handling endware
