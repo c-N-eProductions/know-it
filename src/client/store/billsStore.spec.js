0f92a78825aa4c11ba9766a65725573838257132
Carmen Peralta <cperalta26@gmail.com>
2019-08-01 12:29:20 -0400

    chore(billsStore.spec.js): clean up code

--- src/client/store/billsStore.spec.js
+++ src/client/store/billsStore.spec.js
@@ -8,16 +8,13 @@ import billsReducer, {
   getRecentBills
 } from './billsStore'
 import recentBillsAPIResults from '../testingData/recentBillsData'
-const { results: [{ bills }] } = recentBillsAPIResults
-//console.log(`fetchRecentBillsThunk: ${ fetchRecentBillsThunk}`)
-//console.log(`getRecentBills: ${JSON.stringify(typeof getRecentBills)}`)
 
+const { results: [{ bills }] } = recentBillsAPIResults
 const middlewares = [thunkMiddleware]
 const mockStore = configureMockStore(middlewares)
 
 describe('billsStore - action, thunk, and reducer', () => {
   const initialState = { recentBills: [] }
-  //const store = mockStore(initialState)
   let store = null
   let mockAxios = null
   axios.defaults.baseURL = 'http://localhost:8080'
@@ -28,13 +25,11 @@ describe('billsStore - action, thunk, and reducer', () => {
   })
 
   afterEach(() => {
-    //mockAxios.restore()
     store.clearActions()
   })
 
   describe('getRecentBills action creator', () => {
     it('should create an action to add 20 recent bills', () => {
-      //console.log("store - state: ", store.getState())
       const expectedAction = {
         type: 'GET_RECENT_BILLS',
         fetchedBills: bills
@@ -45,20 +40,6 @@ describe('billsStore - action, thunk, and reducer', () => {
 
   describe('fetchRecentBillsThunk', () => {
     it('eventually dispatches the GET_RECENT_BILLS action', async () => {
-      //console.log(`dispatch function: ${store.dispatch}`)
-      //console.log(`recentBillsThunk: ${JSON.stringify(fetchRecentBillsThunk)}`)
-      /*store.dispatch(fetchRecentBillsThunk())
-        .then(() => {
-          const actions = store.getActions()
-          console.log("store - state: ", store.getState())
-        })*/
-      //const actions = store.getActions()
-
-      //console.log(`actions: ${actions}`)
-      //expect(actions[0].type).to.be.equal('GET_RECENT_BILLS')
-      //expect(actions[0].fetchedBills).to.have.length(2
-
-      //console.log('mockAxios: ', mockAxios.onGet)
       mockAxios.onGet('/api/bills').replyOnce(200, recentBillsAPIResults)
       await store.dispatch(fetchRecentBillsThunk())
       const actions = store.getActions()
@@ -86,4 +67,4 @@ describe('billsStore - action, thunk, and reducer', () => {
       ).to.eql(bills)
     })
   }) // end of reducer sets recent bills on state
-})
+}) // end of billsStore - action, thunk, and reducer
