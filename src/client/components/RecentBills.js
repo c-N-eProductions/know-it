05ec701a5950275a3aa520e05cc428b8d06fcf0d
Carmen Peralta <cperalta26@gmail.com>
2019-12-16 19:45:33 -0500

    feat(RecentBills): add dynamic image to recent bills component
    co-authored-by: Carmen Peralta <cperalta26@gmail.com>
    co-authored-by: Eric Garnett <egcodes@gmail.com>

--- src/client/components/RecentBills.js
+++ src/client/components/RecentBills.js
@@ -16,27 +16,37 @@ export class RecentBills extends Component {
       <div className="recent-bills">
         <h1>20 Most Recent Congressional Bills</h1>
         {recentBills &&
-          recentBills.map(bill => (
-            <div key={bill.number} className="recent-bill">
-              <ul>
-                <li className="bill-number">
-                  <a href={bill.govtrack_url}>{bill.number}</a>
-                </li>
-                <li className="bill-title">{bill.title}</li>
-                <li className="bill-introduced-date">{bill.introduced_date}</li>
-                <li className="bill-sponsor">
-                  {`${bill.sponsor_title} ${bill.sponsor_name}: ${
-                    bill.sponsor_state
-                  }-${bill.sponsor_party}`}
-                </li>
-                <li className="bill-lastest-action">
-                  {`${bill.latest_major_action.slice(0, -1)} on ${
-                    bill.latest_major_action_date
-                  }`}
-                </li>
-              </ul>
-            </div>
-          ))}
+          recentBills.map(bill => {
+            const sponsorId = bill.sponsor_id
+            return (
+              <div key={bill.number} className="recent-bill">
+                <span>
+                  <img
+                    src={`https://theunitedstates.io/images/congress/225x275/${sponsorId}.jpg`}
+                  />
+                </span>
+                <ul style={{ display: 'inline-block' }}>
+                  <li className="bill-number">
+                    <a href={bill.govtrack_url}>{bill.number}</a>
+                  </li>
+                  <li className="bill-title">{bill.title}</li>
+                  <li className="bill-introduced-date">
+                    {bill.introduced_date}
+                  </li>
+                  <li className="bill-sponsor">
+                    {`${bill.sponsor_title} ${bill.sponsor_name}: ${
+                      bill.sponsor_state
+                    }-${bill.sponsor_party}`}
+                  </li>
+                  <li className="bill-lastest-action">
+                    {`${bill.latest_major_action.slice(0, -1)} on ${
+                      bill.latest_major_action_date
+                    }`}
+                  </li>
+                </ul>
+              </div>
+            )
+          })}
       </div>
     )
   }
